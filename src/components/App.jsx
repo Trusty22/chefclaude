3c974ca3d15be13aaf9857c0046da2d668a9009c
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-10-26 22:32:56 -0700

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./Header"
 
 export default function App() {
 
-    return (<h1>Hi</h1>)
+    return (
+        <Header />
+
+    )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import icon from "/Users/mandeepmasoun/Desktop/MyFolders/website/Claude/chefclaude/src/images/chef-claude-icon.png"
+export default function Header() {
+    return (
+
+        <header>
+            <img src={icon} />
+            <h1> Chef Claude</h1>
+        </header>
+
+    )
+}
