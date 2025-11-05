ff81babcc405d3fe9fa8844c931ab9dbe942d7a8
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-11-04 20:22:26 -0800

    before adding added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,36 @@
 export default function Main() {
+
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsList = ingredients.map((item) => (
+        <li key="ingredient">{item}</li>
+
+    ))
+
+    const handelSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+
     return (
 
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handelSubmit} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. Orgeno"
                     aria-label="Add Ingredient"
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientsList}
+
+            </ul>
 
         </main>
     )
